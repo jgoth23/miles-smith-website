eb77459d093d19a26fe5fea5dbb96a64d9889a1c
Johnny Goth <Jgoth23@gmail.com>
2021-05-19 20:36:28 -0500

    google fonts brought in

--- miles-smith-website/src/components/Navbar.js
+++ miles-smith-website/src/components/Navbar.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react'
-import { MenuItems } from "./MenuItems"
+import React, { Component } from 'react';
+import { MenuItems } from './MenuItems';
 
 class Navbar extends Component {
   render() {
-    return(
+    return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">Miles Smith</h1>
-        <div className="menu-icon">
-
-        </div>
+        <div className="menu-icon"></div>
         <ul>
           {MenuItems.map((item, index) => {
-              return (
-                
-              )     
+            return (
+              <li key={index}> 
+                <a className={MenuItems.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
           })}
-          <li><a className={MenuItems.cName}></a></li>
         </ul>
       </nav>
-    )
+    );
   }
 }
 
