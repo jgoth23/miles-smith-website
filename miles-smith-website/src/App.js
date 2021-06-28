8389a9113fe7021ad1a1a7db6dd8a3b457737b18
Johnny Goth <Jgoth23@gmail.com>
2021-06-28 15:00:49 -0500

    made components

--- miles-smith-website/src/App.js
+++ miles-smith-website/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Gallery from './pages/Gallery';
+import Portfolio from './pages/Portfolio';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/contact' component={Contact} />
+        <Route path='/gallery' component={Gallery} />
+        <Route path='/portfolio' component={Portfolio} />
+      </Switch>
+      </Router>
       
     </div>
   );
--- miles-smith-website/src/components/Navbar.js
+++ miles-smith-website/src/components/Navbar.js
@@ -3,22 +3,24 @@ import { MenuItems } from './MenuItems';
 import './Navbar.css';
 
 class Navbar extends Component {
-  state = { clicked: false }
+  state = { clicked: false };
 
   handleClick = () => {
-    this.setState({clicked: !this.state.clicked})
-  }
+    this.setState({ clicked: !this.state.clicked });
+  };
   render() {
     return (
       <nav className="NavbarItems">
         <h1 className="navbar-logo">Miles Smith</h1>
         <div className="menu-icon" onClick={this.handleClick}>
-        <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+          <i
+            className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}
+          ></i>
         </div>
         <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
           {MenuItems.map((item, index) => {
             return (
-              <li key={index}> 
+              <li key={index}>
                 <a className={item.cName} href={item.url}>
                   {item.title}
                 </a>
