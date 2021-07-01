1bd2a21f8feb3f9fae028bb5ca5176fbc1cd5808
Johnny Goth <Jgoth23@gmail.com>
2021-07-01 14:14:40 -0500

    image centered

--- miles-smith-website/src/pages/Home.js
+++ miles-smith-website/src/pages/Home.js
@@ -4,8 +4,8 @@ import React from 'react';
 function Home() {
   return (
     <div className='Home'>
-      <h1>Home</h1>
-      <img src="Vegas Trip-475 copy.jpg"></img>
+      
+      <img src="Vegas Trip-475 copy.jpg" className="homeImage"></img>
     </div>
   )
 }
