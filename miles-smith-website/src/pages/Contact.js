50c2d3e998276955762fb030520c7dda4f1b4390
Johnny Goth <Jgoth23@gmail.com>
2021-07-26 13:21:05 -0500

    changed cursor

--- miles-smith-website/src/pages/Contact.js
+++ miles-smith-website/src/pages/Contact.js
@@ -1,10 +1,14 @@
 import React from 'react';
+// import * as HeroIcons from 'react-icons/hi';
 
 function Contact() {
   return (
     <div>
-      <h1 className='contact'>For inquires write to<a href="mailto:milezander@gmail.com"></a> milezander@gmail.com</h1>
-      <p></p> 
+      <h1 className='contact'>For inquires write to</h1>
+      <div><a className="social-link" href="mailto:milezander@gmail.com">
+        </a> milezander@gmail.com
+        {/* <HeroIcons.HiMail /> */}
+        </div>
     </div>
     
   )
